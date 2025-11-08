b604e55e83367fba5f3ab491feb240d279c7f353
ptsfdtz <pitousanfadetuzi@gmail.com>
2025-11-08 22:47:23 +0800

    init

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Hello, Portfolio</h1>
+    </main>
+  );
+}
---
+++ next.config.ts
@@ -0,0 +1,10 @@
+const nextConfig = {
+  output: 'export',
+  basePath: '/Portfolio',
+  assetPrefix: '/Portfolio',
+  experimental: {
+    appDir: true,
+  },
+};
+
+module.exports = nextConfig;
